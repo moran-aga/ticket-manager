0a18a1905150c5fc4f1a5bf7cd545c81806a45c0
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-24 13:24:02 +0200

    creating routes for url

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const api = require('./routes/api/index');
 
 app.use(express.static("client/build"));
+app.use('/api', api);
+app.use('*', (req, res) => {
+    res.status(404).json({message: 'Page Not Found'});
+})
 
 module.exports = app;
