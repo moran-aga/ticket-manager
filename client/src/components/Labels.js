1e02f99e20ed4103be198d55ba421bebd8d6a34e
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-24 18:45:36 +0200

    Create label component

---
+++ client/src/components/Labels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Labels({labels}) {
+    return (
+        <div className = "labels">
+            {labels.map(label => <span className = "label">{label} </span>)}
+        </div>
+    );
+}
+
+export default Labels;
