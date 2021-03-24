0e46c19a76c7ad5e118ba486cf076044adecf45e
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-03-24 14:30:20 +0200

    get with search Text - not working

--- routes/api/tickets/index.js
+++ routes/api/tickets/index.js
@@ -1,4 +1,27 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const tickets = Router();
+const Ticket = require("../../../models/ticket");
+
+//get all tickets
+tickets.get("/", (req, res) => {
+ Ticket.find({})
+  .then((DBtickets) => {
+   res.json({ DBtickets });
+  })
+  .catch((err) => {
+   res.json({ error: err.message });
+  });
+});
+
+//get ticket with search params
+tickets.get("/", (req, res) => {
+ const query = req.query.searchText;
+ const regex = new RegExp(query);
+ Ticket.find({ title: { $regex: regex, $options: "i" } })
+  .then((titles) => {
+   res.json({ titles });
+  })
+  .catch();
+});
 
 module.exports = tickets;
