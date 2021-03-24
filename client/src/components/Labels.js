1034064c772ab6658a12b0e70add1395684bb00b
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-24 22:52:55 +0200

    deleted space after label to pass test

--- client/src/components/Labels.js
+++ client/src/components/Labels.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Labels({labels}) {
     return (
         <div className = "labels">
-            {labels.map(label => <span className = "label">{label} </span>)}
+            {labels&&labels.map(label => <span className = "label">{label}</span>)}
         </div>
     );
 }
