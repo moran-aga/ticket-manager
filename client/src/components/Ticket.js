13e339b64935d7091368554a2baf1390655bfd35
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-24 20:09:29 +0200

    Update Ticket.js

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -8,7 +8,7 @@ function Ticket({ tickets }) {
                 return <div className = "ticket">
                     <div className="ticket-title">{ticket.title}</div>        
                     <div className = "ticket-content">{ticket.content}</div>        
-                    <div classname ="userEmail">{ticket.userEmail}</div>        
+                    <div className ="userEmail">{ticket.userEmail}</div>        
                     <div className="creation-time">{ticket.creationTime}</div> 
                     <Labels labels = {ticket.labels}/>
                 </div>
