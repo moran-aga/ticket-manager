bab4c7aa54b88c02366453cec45a66f440f59a4f
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-24 18:46:19 +0200

    update the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Ticket from './components/Ticket';
 
 function App() {
-  return ;
+  
+  return (
+    <div className = "container">
+      <Ticket tickets = {tickets}/>
+    </div>
+  );
 }
 
 export default App;
