8208f15cac7f8b10931b47020418518dd78ad357
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-24 15:45:34 +0200

    remove handling for unknown

--- app.js
+++ app.js
@@ -4,8 +4,5 @@ const api = require('./routes/api/index');
 
 app.use(express.static("client/build"));
 app.use('/api', api);
-app.use('*', (req, res) => {
-    res.status(404).json({message: 'Page Not Found'});
-})
 
 module.exports = app;
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,8 +3,5 @@ const api = Router();
 const tickets = require('./tickets/index');
 
 api.use('/tickets', tickets);
-api.use('*', (req, res) => {
-    res.status(404).json({message: 'Page Not Found'});
-})
 
 module.exports = api;
