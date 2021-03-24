73896339161a9c675e90fab34564da976559b2bd
Assaf Kaner <kaner.assaf@gmail.com>
2021-03-24 18:47:27 +0200

    create getTickets from DB function

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import Ticket from './components/Ticket';
+import axios from 'axios';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tickets, setTickets] = useState([]);
   
+  const getTickets = async () => {
+    const { data } = await axios.get('api/tickets');
+    setTickets(data.DbTickets);
+  };
+
   return (
     <div className = "container">
       <Ticket tickets = {tickets}/>
